760c0e776dd02d4d7b4171b19999d499f54590a4
Matan Kotler-Berkowitz <205matan@gmail.com>
2016-09-12 16:48:00 -0400

    Create basic wrapper

---
+++ index.js
@@ -0,0 +1,24 @@
+function handleErrors(response) {
+  return new Promise((resolve, reject) => {
+    if (response.ok) {
+      resolve(response);
+    } else {
+      reject(response.statusText);
+    }
+  })
+}
+
+// factory function that retains original fetch function in closure
+function generatefetchReject(originalFetch) {
+	return function() {url, opts}
+		return originalFetch(url, opts)
+			.then(handleErrors);
+	}
+}
+
+exports.fill = function fill() {
+	window.fetch = generatefetchReject(window.fetch);
+	return true;
+}
+
+exports.fetchReject = generatefetchReject(window.fetch);
